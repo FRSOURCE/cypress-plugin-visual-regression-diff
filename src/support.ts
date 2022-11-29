90c82f5a602e9b4a383998b348838391f2b60d7e
Jakub Freisler <jakub@frsource.org>
2022-11-29 23:39:23 +0100

    feat: support Cypress 11
    closes #187
    
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

--- src/support.ts
+++ src/support.ts
@@ -4,6 +4,7 @@ import { LINK_PREFIX, OVERLAY_CLASS, TASK } from "./constants";
 
 /* c8 ignore start */
 function queueClear() {
+  (cy as unknown as { queue: { reset: () => void } }).queue.reset?.();
   (cy as unknown as { queue: { clear: () => void } }).queue.clear();
   (cy as unknown as { state: (k: string, value: unknown) => void }).state(
     "index",
