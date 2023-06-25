1bbc7207ecc2c5411863b9a51da7e1400473c774
Jakub Freisler <jakub@frsource.org>
2023-06-26 01:51:56 +0200

    chore: test changes
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

--- examples/next/components/about-component.cy.tsx
+++ examples/next/components/about-component.cy.tsx
@@ -1,5 +1,4 @@
 import AboutComponent from './about-component'
-/* eslint-disable */
 // Disable ESLint to prevent failing linting inside the Next.js repo.
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
